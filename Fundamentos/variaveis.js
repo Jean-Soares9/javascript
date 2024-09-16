4c8a2f889b4cd2437bca752a3fa92873f1185616
Jean Anddrade <jean_andrade.s@hotmail.com>
2024-09-16 19:36:32 -0300

    setup do ambiente de desenvolvimento

---
+++ Fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+* Estudo de constante e variavel
+* @author Jean Andrade
+*/
+ 
+console.clear()
+ 
+//variaveis
+let nome 
+let idade 
+let vip
+ 
+nome = "Jean Andrade"
+console.log(typeof(nome))
+console.log(nome)
+ 
+idade= "35"
+console.log(typeof(idade))
+console.log(idade)
+ 
+vip = true 
+console.log(typerof(vip))
+console.log(log)
