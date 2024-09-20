fda822d6178d0d74ad0e5c100627eca60386e346
Jean Anddrade <jean_andrade.s@hotmail.com>
2024-09-19 22:15:39 -0300

    criado ficha de aluno

---
+++ exercicios/academia.js
@@ -0,0 +1,29 @@
+/**
+ * Ficha de um aluno de academia
+ * @autor Jean Andrade
+ */
+
+// variaveis
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+// entrada de dados
+nome = "Jean Andrade"
+idade = 35
+peso = 88
+altura = 1.80
+vip = true
+
+// saida
+console.log("Ficha de aluno")
+console.log("_________________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`Vip: ${vip}`)
