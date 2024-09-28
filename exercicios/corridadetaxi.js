0e83639c71815cc02004981908645a8edcbabb4c
Jean Anddrade <jean_andrade.s@hotmail.com>
2024-09-27 21:29:21 -0300

    Exercicios

---
+++ exercicios/corridadetaxi.js
@@ -0,0 +1,32 @@
+/**
+* Calculo do valor de uma corrida de taxi:
+*Desenvolva um programa que calcule o valor de uma corrida de táxi.
+*O usuário deve fornecer a distância percorrida (km) e o valor fixo por quilômetro.
+*O programa deve calcular e exibir o valor total da corrida.
+ *@autor Jean Andrade
+ */
+
+ // Importar a biblioteca readline-sync
+
+const input = require('readline-sync')
+
+// variaveis
+ 
+let valorc,distancia,valorkm
+ 
+ 
+console.clear()
+console.log("Cálculo do valor de uma corrida de taxi")
+ 
+//entrada
+valorkm = 5
+distancia = Number(input.question("Distancia percorrida: "))
+ 
+ 
+//processamento
+ 
+valorc = valorkm * distancia
+ 
+//saida
+ 
+console.log(`valor total: ${valorc}`)
---
+++ exercicios/media3.js
@@ -0,0 +1,30 @@
+/**
+ *  Calculo da media aritmetica de 3 valores
+ * @autor Jean Andrade
+ */
+
+// Importar a biblioteca readline-sync
+
+const input = require('readline-sync')
+
+// Variaveis
+
+let num1, num2, num3, media
+
+console.clear()
+console.log("Calculo da media aritmetica de 3 numeros")
+
+// entrada
+// No console entradas(input) numéricas precisam ser convertidas com o uso do método Number()
+
+num1 = Number(input.question("Digite o primeiro numero: "))
+num2 = Number(input.question("Digite o segundo numero: "))
+num3 = Number(input.question("Digite o terceiro numero: "))
+
+// processamento
+
+media = (num1 + num2 + num3) / 3
+
+// saída
+
+console.log(`Média: ${media}`)
